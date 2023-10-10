9e16fb98de6647cf504c003199af8fb944537dff
adjohnson23 <johnso23@rose-hulman.edu>
2023-10-10 17:17:52 -0400

    Pygame Tutorial: Render Ball

--- beginning.py
+++ beginning.py
@@ -1,2 +1,21 @@
-# The beginning of the world
-print("hello world!");
+# Going to use pygame for 2D video game
+import sys, pygame
+pygame.init()
+
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("images/ball.png")
+ballrect = ball.get_rect()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
+
