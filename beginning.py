14d35ce409a67bf42a805162e505c9fa246fb43c
adjohnson23 <johnso23@rose-hulman.edu>
2023-10-10 18:31:28 -0400

    Added fps debugger

--- beginning.py
+++ beginning.py
@@ -2,7 +2,6 @@
 import sys, pygame
 pygame.init()
 
-framerate = 30
 size = width, height = pygame.display.get_desktop_sizes()[0][0] / 2, pygame.display.get_desktop_sizes()[0][1] / 2
 speed = [2, 2]
 black = 0, 0, 0
@@ -13,7 +12,10 @@ print("Welcome to the AJStory project. Not much here yet, but I hope you enjoy! 
 pygame.display.init()
 pygame.display.set_caption(title)
 screen = pygame.display.set_mode(size)
-framedelay = int (1000 / framerate)
+runtime = pygame.time.Clock()
+
+# Debug text, for now it'll only show fps
+debugger = pygame.font.Font(None, 20)
 
 player = pygame.image.load("images/diger.png")
 playerrect = player.get_rect()
@@ -22,14 +24,21 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    playerrect = playerrect.move(speed)
+    playerrect = playerrect.move(speed) # Move around the screen
     if playerrect.left < 0 or playerrect.right > width:
         speed[0] = -speed[0]
     if playerrect.top < 0 or playerrect.bottom > height:
         speed[1] = -speed[1]
 
+    # Update the debugger fps
+    debugtext = "fps: " + str(runtime.get_fps())
+    debugsurface = debugger.render(debugtext, False, (255, 255, 255))
+    debugrect = debugsurface.get_rect()
+
     screen.fill(black)
     screen.blit(player, playerrect)
+    screen.blit(debugsurface, debugrect)
     pygame.display.flip()
-    pygame.time.wait(framedelay)
+    runtime.tick(30) # Run at 30 frames a second
+
 
